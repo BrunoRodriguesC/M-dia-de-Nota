8f83a327556b33c9a929b779d38bd1fb36ed449b
Bruno Rodrigues Da Silva <Brunochh@outlook.com>
2022-11-18 18:46:34 -0300

    Add files via upload

---
+++ Media_de_notas.py
@@ -0,0 +1,6 @@
+n1=float(input('Primeira nota do aluno: '))
+n2=float(input('Segunda nota do aluno: '))
+n3=float(input('Terceira nota do aluno: '))
+n4=float(input('Quarta nota do aluno: '))
+m= (n1 + n2 + n3 + n4) /4
+print(f'A média das notas é {m:.1f}')
